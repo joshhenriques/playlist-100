078286cd1116ae3f645e53e3767f1a65c9325d25
joshhenriques <85077479+joshhenriques@users.noreply.github.com>
2023-07-02 14:42:59 -0400

    Added templates

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
-from flask import Flask, url_for, session, request, redirect
+from flask import Flask, url_for, session, request, redirect, render_template_string, render_template
+import urllib.request
 import json
 import time
+import os, shutil
 import pandas as pd
 from dataProcessing import makeData
 from config import cid, secret
@@ -11,11 +13,24 @@ app = Flask(__name__)
 
 app.secret_key = 'OIUHs9w8zqJSHd112' #Random string for session
 app.config['SESSION_COOKIE_NAME'] = 'Joshs Cookie'
+app.config['UPLOAD_FOLDER'] = os.path.join('static' , 'Images')
 TOKEN_INFO = "token_info"
 
 @app.route('/')
 def index():
-    #Add Login button
+    #Add Login buttoN
+
+    #Removes images in folder
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
     sp = create_spotify_oauth()
     auth_url = sp.get_authorize_url()
 
@@ -28,6 +43,7 @@ def redirectPage():
     code = request.args.get('code')
     token_info = sp.get_access_token(code)
     session[TOKEN_INFO] = token_info
+
     return redirect(url_for('getData', _external = True))
 
 @app.route('/getData')
@@ -40,8 +56,70 @@ def getData():
     
     #Iterate to get multiple songs
     sp = spotipy.Spotify(auth = token_info['access_token'])
+    uris = makeData(sp)
+    import random
 
-    return makeData(sp)
+    random.shuffle(uris)
+    uris = uris[:10]
+    session['uris'] = uris
+
+    return redirect(url_for('songs', uris = uris, _external = True))
+
+@app.route('/songs')
+def songs():
+    try:
+        token_info = get_token()
+    except:
+        print("User not logged in")
+        return redirect("/")
+
+    sp = spotipy.Spotify(auth = token_info['access_token'])
+    uris = request.args['uris']
+    uris = session['uris']
+    
+    trackURLs = []
+    imageURLS = []
+    imagePaths = []
+    trackNames = []
+    artists=[]
+    for uri in uris:
+        trackURLs.append("https://open.spotify.com/track/" + uri)
+        
+        track = sp.track(track_id=uri)
+        trackName = track['name']
+        trackNames.append(trackName)
+
+        imageURL = track['album']['images'][1]['url']
+        imageURLS.append(imageURL)
+        imageName = "album" + uri
+        imagePath = os.path.join(app.config['UPLOAD_FOLDER'], imageName)
+        imagePaths.append(imagePath)
+        urllib.request.urlretrieve(imageURL, imagePath)
+        # print(track['album']['images'])
+
+        
+        artist = ""
+        for i in range(len(track['artists'])):
+            artist += track['artists'][i]['name'] + ', '
+        artist = artist.rsplit(',' , 1)[0]
+        artists.append(artist)
+        
+        # print(trackName)
+        # print(artists)
+    
+    return render_template(
+        "songs.html",  
+        albumImg1 = imagePaths[0], link1 = trackURLs[0], name1 = trackName[0], artist1 = artists[0],
+        albumImg2 = imagePaths[1], link2 = trackURLs[1], name2 = trackName[1], artist2 = artists[1],
+        albumImg3 = imagePaths[2], link3 = trackURLs[2], name3 = trackName[2], artist3 = artists[2],
+        albumImg4 = imagePaths[3], link4 = trackURLs[3], name4 = trackName[3], artist4 = artists[3],
+        albumImg5 = imagePaths[4], link5 = trackURLs[4], name5 = trackName[4], artist5 = artists[4],
+        albumImg6 = imagePaths[5], link6 = trackURLs[5], name6 = trackName[5], artist6 = artists[5],
+        albumImg7 = imagePaths[6], link7 = trackURLs[6], name7 = trackName[6], artist7 = artists[6],
+        albumImg8 = imagePaths[7], link8 = trackURLs[7], name8 = trackName[7], artist8 = artists[7],
+        albumImg9 = imagePaths[8], link9 = trackURLs[8], name9 = trackName[8], artist9 = artists[8],
+        albumImg10= imagePaths[9], link10 = trackURLs[9],name10= trackName[9], artist10 = artists[9],
+    )
 
 def get_token():
     token_info = session.get(TOKEN_INFO, None)
--- dataProcessing.py
+++ dataProcessing.py
@@ -1,11 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import pandas as pd
-from tabulate import tabulate
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity  
 from sklearn.metrics.pairwise import sigmoid_kernel
 import numpy as np
+
+numOfSongs = 50
+popFactor = 0.2
+genreFactor = 0.05
 
 def makeData(sp):
 
@@ -54,6 +57,9 @@ def makeData(sp):
         #     lt_uris += [item['uri'].split(':')[-1]]
         #     lt_features += [sp.audio_features(item['uri'].split(':')[-1])]
         for item in medium_term: 
+            # print(item)
+            # print("")
+            # print("")
             artistName = item['album']['artists'][0]['name'] 
             track = sp.search(artistName)['tracks']['items'][0]
             artist = sp.artist(track["artists"][0]["external_urls"]["spotify"])
@@ -70,7 +76,7 @@ def makeData(sp):
     # genres.sort(key=Counter(genres).get, reverse=True)
     genreDict = {}
     for genre in set(genres):
-        genreDict[genre] = genres.count(genre)
+        genreDict[genre.replace(' ', '-')] = genres.count(genre)
 
     mt_dataFrame = pd.DataFrame(mt_features)
     mt_dataFrame = mt_dataFrame.drop(mt_dataFrame.columns[14:], axis=1)  #Holds the data to reccomend song
@@ -83,16 +89,20 @@ def makeData(sp):
 def genRecommendations(df, genreDict):
 
     #Organize CSV data to have same format as df
-    # spotify_df = pd.read_csv('tracks.csv')
-    # spotify_df = spotify_df.drop(spotify_df.columns[19], axis = 1)
-    # spotify_df = spotify_df.drop(spotify_df.columns[1:8], axis = 1)
-
     spotify_df = pd.read_csv('dataset.csv')
+    spotify_df = spotify_df.drop_duplicates(subset=['track_id'])
+    #Drop songs that have genre that do not allign with users
+
+    # print(genreDict)
+    spotify_df = spotify_df[spotify_df['track_genre'].str.contains('|'.join(key for key in genreDict.keys()))]
     datasetGenres = spotify_df['track_genre']
+
+    popularity = spotify_df['popularity']
     spotify_df = spotify_df.drop(spotify_df.columns[2:8], axis = 1)
     spotify_df = spotify_df.drop(spotify_df.columns[0], axis = 1)
     spotify_df = spotify_df.drop(spotify_df.columns[12:], axis = 1)
     spotify_df = spotify_df.rename(columns={'track_id' : 'uri'})
+
 
     feature_cols=['danceability','energy','key', 'loudness', 'mode','speechiness','acousticness',
                   'instrumentalness', 'liveness', 'valence','tempo']
@@ -100,17 +110,23 @@ def genRecommendations(df, genreDict):
     #Scale Data
     scaler = MinMaxScaler()
     df[feature_cols] = scaler.fit_transform(df[feature_cols])
+    popularity = scaler.fit_transform(popularity.values.reshape(-1, 1))
     spotify_df[feature_cols] = scaler.fit_transform(spotify_df[feature_cols])
 
     weightedVector = genWeightedVector(df)
-    
-    # df.info()
-    # Create cosine model
 
-    spotify_df['sim'] = cosine_similarity(spotify_df[feature_cols], weightedVector.reshape(1,-1))
-    topSongs = spotify_df.sort_values('sim', ascending=False).head(10)
+    # Create cosine model
+    spotify_df['sim'] = popularity
+    for index, row in spotify_df.iterrows():
+        spotify_df.at[index, 'sim'] =  cosine_similarity(np.array(row[feature_cols]).reshape(1,-1), weightedVector.reshape(1,-1)) + spotify_df.at[index, 'sim']*popFactor
+        try:
+            spotify_df.at[index, 'sim'] = spotify_df.at[index, 'sim'] + genreDict[datasetGenres[index]] * genreFactor
+        except:
+            continue
+
+    topSongs = spotify_df.sort_values('sim', ascending=False).head(numOfSongs)
    
-    return str(topSongs['uri'])
+    return topSongs['uri'].tolist()
 
 def genWeightedVector(df):
 
@@ -130,22 +146,24 @@ def genWeightedVector(df):
 
 spotify_df = pd.read_csv('dataset.csv')
 # spotify_df.info()
-spotify_df = spotify_df.drop(spotify_df.columns[2:8], axis = 1)
-spotify_df = spotify_df.drop(spotify_df.columns[0], axis = 1)
-spotify_df = spotify_df.drop(spotify_df.columns[12:], axis = 1)
-spotify_df = spotify_df.rename(columns={'track_id' : 'uri'})
-# spotify_df = spotify_df.drop(spotify_df.columns[19], axis = 1)
-# spotify_df = spotify_df.drop(spotify_df.columns[19], axis = 1)
-# spotify_df = spotify_df.drop(spotify_df.columns[1:8], axis = 1)
+# spotify_df = spotify_df.drop(spotify_df.columns[2:8], axis = 1)
+# spotify_df = spotify_df.drop(spotify_df.columns[0], axis = 1)
+# spotify_df = spotify_df.drop(spotify_df.columns[12:], axis = 1)
+# spotify_df = spotify_df.rename(columns={'track_id' : 'uri'})
+# # spotify_df = spotify_df.drop(spotify_df.columns[19], axis = 1)
+# # spotify_df = spotify_df.drop(spotify_df.columns[19], axis = 1)
+# # spotify_df = spotify_df.drop(spotify_df.columns[1:8], axis = 1)
 
-feature_cols=['danceability','energy','key', 'loudness', 'mode','speechiness','acousticness',
-                  'instrumentalness', 'liveness', 'valence','tempo']
-import random
-vector = []
-for r in range(11):
-    vector.append(random.random())
+# feature_cols=['danceability','energy','key', 'loudness', 'mode','speechiness','acousticness',
+#                   'instrumentalness', 'liveness', 'valence','tempo']
+# import random
+# vector = []
+# for r in range(11):
+#     vector.append(random.random())
 
-spotify_df['sim'] = cosine_similarity(spotify_df[feature_cols], np.array(vector).reshape(1,-1))
-topSongs = spotify_df.sort_values('sim', ascending=False).head(5)
+# spotify_df['sim'] = cosine_similarity(spotify_df[feature_cols], np.array(vector).reshape(1,-1))
+# topSongs = spotify_df.sort_values('sim', ascending=False).head(5)
 
 # print(topSongs)
+# for index, row in spotify_df.iterrows():
+#     print(row['track_genre'])
